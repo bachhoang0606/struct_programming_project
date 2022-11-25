990d673e31c0186d008df4ced22edd0ad356e4ee
bachhoang0606 <0606bach@gmail.com>
2022-11-25 18:53:22 +0700

    some thing

--- struct_programming/resources/views/vouchers/index.blade.php
+++ struct_programming/resources/views/vouchers/index.blade.php
@@ -2,4 +2,12 @@
 
 @section('content')
     <h1>Heare is the index page <br>{{$data}}</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-3 bg-primary text-white">.col</div>
+            <div class="col-3 p-3 bg-dark text-white">.col</div>
+            <div class="col-3 p-3 bg-primary text-white">.col</div>
+          </div>
+    </div>
+    
 @endsection
