10d510c9734000065c5babb43417400a29b6a55e
Hoang Anh <anh.lnh194471@sis.hust.edu.vn>
2022-11-30 23:10:11 +0700

    fix variable name

--- struct_programming/resources/views/vouchers/create.blade.php
+++ struct_programming/resources/views/vouchers/create.blade.php
@@ -28,12 +28,12 @@
     <p></p>
     <div class="form-group">
         <label for="minimun_price" class="form-label">Minimum price</label><br>
-        <input type="number" name="minimun_price" min="0" class="form-control" required>
+        <input type="number" name="minimun_price" value="0" min="0" class="form-control" required>
     </div>
     <p></p>
     <div class="form-group">
         <label for="quantium" class="form-label">Amount of vouchers:</label><br>
-        <input type="number" name="quantium" min="1" class="form-control" required>
+        <input type="number" name="quantium" value="1" min="1" class="form-control" required>
     </div>
     <p></p>    
     <div class="form-group">
@@ -41,76 +41,57 @@
         <input type="date" name="outdate_at" class="form-control" required>
     </div>
     <p></p>
+    <div class="form-group">
+        <label for="price" id="label_price">Price: </label>
+        <input type="number" id="input_price" name="price" value="0" min="0" class="form-control" required>
+    </div>
     <h4>Choose types for voucher:</h4>
     <div class="form-check">
-        <input class="form-check-input" type="radio" id="free_ship" name="Vtype" value="freeships" onclick="freeShipOption();perDisOption();priceDisOption()">
+        <input class="form-check-input" type="radio" id="free_ship" name="Vtype" value="freeships" onclick="perDisOption()" required>
         <label class="form-check-label" for="freeships">Free ship</label>
     </div>
-    <div class="form-group">
-        <label for="FP_price" id="label_FP_price" style="display:none">Price: </label>
-        <input type="number" id="FP_price" name="FP_price" value="0" style="display:none" min="0" class="form-control">
-    </div>
     <div class="form-check">
-        <input class="form-check-input" type="radio" id="percent_discount" name="Vtype" value="percentDiscounts" min="0" onclick="perDisOption();freeShipOption();priceDisOption()">
+        <input class="form-check-input" type="radio" id="percent_discount" name="Vtype" value="percentDiscounts" min="0" onclick="perDisOption()">
         <label class="form-check-label" for="percentDiscounts">Percent discount</label>
     </div>
     <div class="form-group">
         <label for="max_price" name="label_perDis" style="display:none">Set max price: </label>
-        <input type="number" id="set_max" name="input_perDis_1" value="0" min="0" style="display:none" class="form-control">
+        <input type="number" id="set_max" name="max_price" value="0" min="0" style="display:none" class="form-control">
     </div>
     <div class="form-group">
         <label for="percent" name="label_perDis" style="display:none">Set percentage: </label>
-        <input type="number" id="set_percent" name="input_perDis_2" value="0" min="0" max="100" style="display:none" class="form-control">
+        <input type="number" id="set_percent" name="percent" value="0" min="0" max="100" style="display:none" class="form-control">
     </div>
     <div class="form-check">
-        <input class="form-check-input" type="radio" id="price_discount" name="Vtype" value="priceDiscounts" onclick="priceDisOption();freeShipOption();perDisOption()">
+        <input class="form-check-input" type="radio" id="price_discount" name="Vtype" value="priceDiscounts" onclick="perDisOption()">
         <label class="form-check-label" for="priceDiscounts">Price discount</label>
-    </div>
-    <div class="form-group">
-        <label for="input_priceDis" id="label_priceDis" style="display:none">Price: </label>
-        <input type="number" id="input_priceDis" name="input_priceDis" value="0" min="0" style="display:none" class="form-control">
     </div>
     <h4>Choose range of products:</h4>
     <div class="form-check">
-        <input class="form-check-input" type="radio" id="VGeneral" name="Vtype" value="VGeneral" onclick="vGeneralOption()">
+        <input class="form-check-input" type="radio" id="VGeneral" name="Vrange" value="VGeneral" onclick="vGeneralOption()" required>
         <label class="form-check-label" for="VGeneral">Available for all product</label>
     </div>
     <div class="form-check">
-        <input class="form-check-input" type="radio" id="VProduct" name="Vtype" value="VProduct" onclick="vProductOption()">
+        <input class="form-check-input" type="radio" id="VProduct" name="Vrange" value="VProduct" onclick="vProductOption()">
         <label class="form-check-label" for="VProduct">Available for an amount of products</label>
     </div>
     <div class="form-group">
         <label for="set_of_products" id="label_Vproduct" style="display:none">Please enter product's names: </label>
         <input type="text" id="input_Vproduct" name="set_of_products" style="display:none" class="form-control">
     </div>
-    <p></p><input type="submit" value="Submit" class="btn btn-primary">
+    <p></p><input type="submit" value="Submit" class="btn btn-primary" onclick="perDisOption()">
 </form>
 
 @endsection
 
 <script>
-    function freeShipOption(){
-        let checkbox = document.getElementById('free_ship');
-        let label = document.getElementById('label_FP_price');
-        let input = document.getElementById('FP_price');
-
-        if (checkbox.checked){
-            label.style.display = "block";
-            input.style.display = "block";
-            input.required = true;
-        }else{
-            label.style.display = "none";
-            input.style.display = "none";
-            input.required = false;
-            input.value=0;
-        }
-    }
+    
 
     function perDisOption(){
         let checkbox = document.getElementById('percent_discount');
         let label = document.getElementsByName('label_perDis');
         let input1 = document.getElementById('set_max');
-        let input2 = document.getElementById('set_percent')
+        let input2 = document.getElementById('set_percent');
 
         if (checkbox.checked){
             label[0].style.display = "block";
@@ -119,6 +100,8 @@
             input2.style.display = "block";
             input1.required = true;
             input2.required = true;
+            
+            document.getElementById('input_price').disabled = true;
         }else{
             label[0].style.display = "none";
             label[1].style.display = "none";
@@ -129,23 +112,8 @@
 
             input1.required = false;
             input2.required = false;
-        }
-    }
 
-    function priceDisOption(){
-        let checkbox = document.getElementById('price_discount');
-        let label = document.getElementById('label_priceDis');
-        let input = document.getElementById('input_priceDis');
-
-        if (checkbox.checked){
-            label.style.display = "block";
-            input.style.display = "block";
-            input.required = true;
-        }else{
-            label.style.display = "none";
-            input.style.display = "none";
-            input.value=0;
-            input.required = false;
+            document.getElementById('input_price').disabled = false;
         }
     }
 
