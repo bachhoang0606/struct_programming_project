4e4d270e25a11be6c0ddc567e79b5e2860bf2005
Luu Hiep <luuhiep2309@gmail.com>
2022-11-30 14:54:48 +0700

    commit before pull main

--- struct_programming/resources/views/layouts/layouts.blade.php
+++ struct_programming/resources/views/layouts/layouts.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Document</title>
     <style>
         * {
@@ -119,7 +120,7 @@
 
 
             </div>
-            <div class="col-md-7 aside">
+            <div class="col-md-7 aside no-padding">
                 @yield('content')
 
             </div>
--- struct_programming/resources/views/vouchers/index.blade.php
+++ struct_programming/resources/views/vouchers/index.blade.php
@@ -1,24 +1,86 @@
 @extends('layouts.layouts')
 @section('content')
+<?php  
+    // foreach ($voucher_list as $voucher){
+    //     print '<div class="container-voucher">
+    //         <div class="voucher-name">';
+    //     print "<span>";
+    //         if($voucher->price)
+    //             print "freeship";
+    //         else {
+    //                 print "discount";
+    //         }
+    //     print "</span>";
+    //     print '</div>
+    //     <div class="voucher-body">
+    //         <div class="voucher-conent">
+    //             <h3>don hang dau tien</h3>
+    //             <h4>co hieu luc tu ';
+    //         $voucher->created_at;
+    //      print'</h4>
+    //         </div>
+    //        <a class="link">chi tiet</a>
+    //     </div>
 
-    <div class="container-voucher">
-        <div class="voucher-name">
-            <span>Mien phi van chuyen</span>
-        </div>
-        <div class="voucher-body">
-            <div class="voucher-conent">
-                <h3>don hang dau tien</h3>
-                <h4>khong gioi han gia tri khuyen mai toi da</h4>
-                <h4>co hieu luc tu 21.11</h4>
-            </div>
-           <a class="link">chi tiet</a>
-        </div>
-
-    </div>
-    @foreach ($voucher_list as $item)
+    // </div>';
+    // }
+    // print "$voucher_list";
+?>
+    {{-- @foreach ($voucher_list as $item)
         @php
-            dd($item);
+            //dd($item);
         @endphp
-    @endforeach
+    @endforeach --}}
     
+    <div>
+        <table>
+            <th>ID</th>
+            <th>Type</th>
+            <th>Price</th>
+            <th>Percent</th>
+            <th>MaxPrice</th>
+            <th>Ngay tao</th>
+            <th>Ngay sua</th>
+            <?php
+                foreach ($voucher_list as $voucher){
+                    if($voucher->price){
+                        $type = "freship";
+                        $price = $voucher->price;
+                        $precent = "none";
+                        $max_price = "none";
+                    }else{
+                        $type = "disscount";
+                        $price = "none";
+                        $precent = $voucher->percent;
+                        $max_price = $voucher->max_price;
+                    }
+                    if($voucher->created_at){
+                        $created = $voucher->created_at;
+                    }else
+                        $created = "null";
+                    if($voucher->created_at){
+                        $updated = $voucher->updated_at;
+                    }else
+                        $updated = "null";
+                    print '<tr>';
+                    print"<td>$voucher->voucher_id</td>";
+                    print"<td>$type</td>";
+                    print"<td>$price</td>";
+                    print"<td>$precent</td>";
+                    print"<td>$max_price</td>";
+                    print"<td>$created</td>";
+                    print"<td>$updated</td>";
+                    print '</tr>';
+                }
+            ?>
+            {{-- <tr>
+                <td>1</td>
+                <td>freeship</td>
+                <td>12000</td>
+                <td>23/09/2001</td>
+                <td>null</td>
+            </tr> --}}
+        </table>
+    </div>
+
 @endsection
