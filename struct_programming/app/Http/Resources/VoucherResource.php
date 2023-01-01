b994fab25880efb9a20041dd0c7b3d0bd13cd7a5
bachhoang0606 <0606bach@gmail.com>
2023-01-01 22:56:33 +0700

    add type column to voucher table and new api uservoucher

--- struct_programming/app/Http/Resources/VoucherResource.php
+++ struct_programming/app/Http/Resources/VoucherResource.php
@@ -20,7 +20,9 @@ class VoucherResource extends JsonResource
             'content' => $this->content,
             'minimun_price' => $this->minimun_price,
             'quantium' => $this->quantium,
+            'effective date' => $this->effective_date,
             'expiration_date' => $this->expiration_date,
+            'type' => $this->type,
         ];
     }
 }
