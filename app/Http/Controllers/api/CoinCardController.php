8a788822a107e56a80ec71ec329fdf952a6fc808
hiep464 <60540818+hiep464@users.noreply.github.com>
2023-01-08 01:12:44 +0700

    update auto load

--- app/Http/Controllers/api/CoinCardController.php
+++ app/Http/Controllers/api/CoinCardController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CoinCardResource;
 use App\Models\CoinCard;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class CoinCardController extends Controller
 {
@@ -33,47 +32,4 @@ class CoinCardController extends Controller
         
     }
 
-    public function createUserPoin(){
-        $response = Http::timeout(10)->get("https://63b964c03329392049f25570.mockapi.io/api/v1/products");
-        $users = $response;
-        if(!is_array($response))
-            $users = json_decode($response, true);
-        $userCoins = CoinCard::all();
-        $userIds = [];
-        $userCoinIds = [];
-        $add = [];
-        $delete = [];
-
-        //coin and discount default
-        $coin = 0;
-
-        foreach($users as $element){
-            $userIds[] = $element["id"];
-        }
-
-        foreach($userCoins as $element){
-            $userCoinIds[] = $element["user_id"];
-        }
-
-        foreach($users as $user){
-            if(!in_array($user["id"], $userCoinIds)){ // add
-                $add[] = CoinCard::insert(['user_id' => $user["id"],
-                                                'name' => $user["name"],
-                                                'phone' => $user["phoneNumber"],
-                                                'email' => $user["email"],
-                                                'coin' => $coin,
-                                                'created_at' => NULL,
-                                                'updated_at' => NULL,]);
-            }
-        }
-
-        foreach($userCoins as $userCoin){
-            if(!in_array($userCoin["user_id"], $userIds)){ // delete
-                $delete[] = CoinCard::find($userCoin["user_id"])->delete();
-            }
-        }
-        $result = ["add" => $add, "delete" => $delete];
-
-        return $result;
-    }
 }
--- app/Http/Controllers/api/ProductAttributeApiController.php
+++ app/Http/Controllers/api/ProductAttributeApiController.php
@@ -5,9 +5,6 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductAttributeResource;
 use App\Models\ProductAttribute;
-use Illuminate\Support\Facades\Http;
-
-use Carbon\Carbon;
 
 class ProductAttributeApiController extends Controller
 {
@@ -21,45 +18,5 @@ class ProductAttributeApiController extends Controller
     public function index(){
         $products = ProductAttribute::all();
         return ProductAttributeResource::collection($products);
-    }
-
-    public function productCoin(){
-        $response = Http::timeout(10)->get("https://p01-product-api-production.up.railway.app/api/user/products");
-        $products = $response["data"];
-        $productsAttrs = ProductAttribute::all();
-        $productIds = [];
-        $productAttrIds = [];
-        $add = [];
-        $delete = [];
-
-        //coin and discount default
-        $coin = 0;
-        $discount = 0;
-        foreach($products as $element){
-            $productIds[] = $element["id"];
-        }
-
-        foreach($productsAttrs as $element){
-            $productAttrIds[] = $element["product_id"];
-        }
-
-        foreach($products as $product){
-            if(!in_array($product["id"], $productAttrIds)){ // add
-                $add[] = ProductAttribute::insert(['product_id' => $product["id"],
-                                                'name' => $product["name"],
-                                                'coin' => $coin,
-                                                'discount' => $discount,
-                                                'created_at' => $product["created_at"] ? Carbon::parse($product["created_at"])->toDateTimeString() : NULL,
-                                                'updated_at' => $product["updated_at"] ? Carbon::parse($product["updated_at"])->toDateTimeString() : NULL,]);
-            }
-        }
-
-        foreach($productsAttrs as $productsAttr){
-            if(!in_array($productsAttr["product_id"], $productIds)){ // delete
-                $delete[] = ProductAttribute::find($productsAttr["product_id"])->delete();
-            }
-        }
-        $result = ["add" => $add, "delete" => $delete];
-        return $result;
     }
 }
