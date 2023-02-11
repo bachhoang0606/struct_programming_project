aea16a4c42092ebdf829c0f06be25e1bd75b04b2
LENOVO <giaopham16092001@gmail.com>
2023-02-11 22:26:52 +0700

    update comment

--- app/Repositories/DashbroardRepository.php
+++ app/Repositories/DashbroardRepository.php
@@ -28,11 +28,11 @@ class DashbroardRepository extends VoucherRepository implements DashbroardReposi
     }
     //get the number of vouchers generated on the days of the month
     public function voucherCreatedInMonth(){
-        $users =  Voucher::select(DB::raw("SUM(total) as count"), DB::raw("EXTRACT(day FROM created_at) as day_name"))
+        $voucher =  Voucher::select(DB::raw("SUM(total) as count"), DB::raw("EXTRACT(day FROM created_at) as day_name"))
             ->whereMonth('created_at', date('m'))
             ->groupBy(DB::raw("EXTRACT(DAY FROM created_at)"))
             ->pluck('count', 'day_name');
-        return $users;
+        return $voucher;
     }
     //get the number of vouchers created and used of the voucher types
     public function getTotalUsed(){
